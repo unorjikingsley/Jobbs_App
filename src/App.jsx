d9301d740a070feb3110a5d0503bd960fbc00f10
unorji kingsley <unorjikingsley@gmail.com>
2023-11-16 20:52:46 +0100

    Basic route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Home</h1>,
+  },
+  {
+    path: '/about',
+    element: <h1>about</h1>,
+  },
+  {
+    path: '/contact',
+    element: <h1>contact</h1>,
+  },
+])
+
 const App = () => {
-  return (
-    <h1>App</h1>
-  )
+  return <RouterProvider router={router} />;
 }
 export default App
