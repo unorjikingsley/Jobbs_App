9e00de0d658e13db5e5a035f8dff9939b92e11a4
unorji kingsley <unorjikingsley@gmail.com>
2023-11-16 21:44:59 +0100

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from "./pages";
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>Home</h1>,
-  },
-  {
-    path: '/about',
-    element: <h1>about</h1>,
-  },
-  {
-    path: '/contact',
-    element: <h1>contact</h1>,
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+      },
+    ],
   },
 ])
 
