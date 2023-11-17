afc2ccb07d0e11ae5416c126a140e7f1473e9307
unorji kingsley <unorjikingsley@gmail.com>
2023-11-17 08:07:05 +0100

    Styled component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,17 @@
+import styled from 'styled-components';
+
+const StyledBtn = styled.button`
+  font-size: 1.5rem;
+  background: red;
+  color: white;
+`;
+
 const Landing = () => {
   return (
-    <h1>Landing</h1>
+    <div>
+      <h1>Landing</h1>
+      <StyledBtn>styled btn</StyledBtn>
+    </div>
   )
 }
 export default Landing
