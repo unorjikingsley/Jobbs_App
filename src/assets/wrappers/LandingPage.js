af581691c6708cac2fa5c1e8616d4a7492bb1be6
unorji kingsley <unorjikingsley@gmail.com>
2023-11-17 18:29:21 +0100

    Landing page design

--- src/assets/wrappers/LandingPage.js
+++ src/assets/wrappers/LandingPage.js
@@ -9,12 +9,14 @@ const Wrapper = styled.section`
     display: flex;
     align-items: center;
   }
+
   .page {
     min-height: calc(100vh - var(--nav-height));
     display: grid;
     align-items: center;
     margin-top: -3rem;
   }
+
   h1 {
     font-weight: 700;
     span {
@@ -22,29 +24,36 @@ const Wrapper = styled.section`
     }
     margin-bottom: 1.5rem;
   }
+
   p {
     line-height: 2;
     color: var(--text-secondary-color);
     margin-bottom: 1.5rem;
     max-width: 35em;
   }
+
   .register-link {
     margin-right: 1rem;
   }
+
   .main-img {
     display: none;
   }
+
   .btn {
     padding: 0.75rem 1rem;
   }
+
   @media (min-width: 992px) {
     .page {
       grid-template-columns: 1fr 400px;
       column-gap: 3rem;
     }
+
     .main-img {
       display: block;
     }
   }
 `;
+
 export default Wrapper;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,17 +1,35 @@
-import styled from 'styled-components';
-
-const StyledBtn = styled.button`
-  font-size: 1.5rem;
-  background: red;
-  color: white;
-`;
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-      <StyledBtn>styled btn</StyledBtn>
-    </div>
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            Jobify is an intuitive job application tracker designed to
+            streamline your job search. Stay organized with a user-friendly
+            interface that allows you to log and monitor your job applications
+            effortlessly.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="Job hunt" className='img main-img' />
+      </div>
+    </Wrapper>
   )
-}
+};
 export default Landing
